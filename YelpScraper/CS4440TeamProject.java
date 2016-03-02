bff9797af505790191710ccaafd4e1d1f13bb2a6
Jongyeon Kim <mattina401@gmail.com>
2016-03-02 10:54:34 -0500

    Update CS4440TeamProject.java

--- YelpScraper/CS4440TeamProject.java
+++ YelpScraper/CS4440TeamProject.java
@@ -12,10 +12,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Keun Lee
- */
+
 public class CS4440TeamProject extends Application {
 
     
